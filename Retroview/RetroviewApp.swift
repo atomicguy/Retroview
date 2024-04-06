e545c172939cf5253df66975a223394dde66dcca
Adam Schuster <adam@atompowered.net>
2024-04-06 16:23:38 -0700

    Initial Commit

---
+++ Retroview/RetroviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroviewApp.swift
+//  Retroview
+//
+//  Created by Adam Schuster on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RetroviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
